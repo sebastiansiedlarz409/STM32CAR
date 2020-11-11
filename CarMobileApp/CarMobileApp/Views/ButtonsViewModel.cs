88534cab569e554e48fc3d1e11961c518a0d3c28
Sebastian Siedlarz <siedlarzsebastian409@gmail.com>
2020-11-11 09:03:20 +0100

    Add label with X Y Z

--- CarMobileApp/CarMobileApp/Views/ButtonsViewModel.cs
+++ CarMobileApp/CarMobileApp/Views/ButtonsViewModel.cs
@@ -7,9 +7,9 @@ namespace CarMobileApp.Views
 {
     public class ButtonsViewModel : INotifyPropertyChanged
     {
-        private double X = 0;
-        private double Y = 0;
-        private double Z = 0;
+        private double Xvalue;
+        private double Yvalue;
+        private double Zvalue;
 
         private INavigation navigation;
 
@@ -30,6 +30,10 @@ namespace CarMobileApp.Views
             UpCommand = new Command(Up);
             DownCommand = new Command(Down);
             SwitchViewCommand = new Command(async () => await Switch());
+
+            X = 0;
+            Y = 0;
+            Z = 0;
         }
 
         public ButtonsViewModel(INavigation navigation) : this()
@@ -48,6 +52,36 @@ namespace CarMobileApp.Views
             get
             {
                 return $"X: {X} Y: {Y} Z: {Z}";
+            }
+        }
+
+        public double X 
+        { 
+            get => Xvalue;
+            set
+            {
+                Xvalue = value;
+                OnPropertyChanged(nameof(PrintValues));
+            }
+        }
+
+        public double Y 
+        { 
+            get => Yvalue;
+            set
+            {
+                Yvalue = value;
+                OnPropertyChanged(nameof(PrintValues));
+            }
+        }
+
+        public double Z
+        {
+            get => Zvalue;
+            set
+            {
+                Zvalue = value;
+                OnPropertyChanged(nameof(PrintValues));
             }
         }
 
--- CarMobileApp/CarMobileApp/Views/SensorViewModel.cs
+++ CarMobileApp/CarMobileApp/Views/SensorViewModel.cs
@@ -7,9 +7,9 @@ namespace CarMobileApp.Views
 {
     public class SensorViewModel : INotifyPropertyChanged
     {
-        private double X = 0;
-        private double Y = 0;
-        private double Z = 0;
+        private double Xvalue = 0;
+        private double Yvalue = 0;
+        private double Zvalue = 0;
 
         private INavigation navigation;
 
@@ -39,6 +39,36 @@ namespace CarMobileApp.Views
             get
             {
                 return $"X: {X} Y: {Y} Z: {Z}";
+            }
+        }
+
+        public double X
+        {
+            get => Xvalue;
+            set
+            {
+                Xvalue = value;
+                OnPropertyChanged(nameof(PrintValues));
+            }
+        }
+
+        public double Y
+        {
+            get => Yvalue;
+            set
+            {
+                Yvalue = value;
+                OnPropertyChanged(nameof(PrintValues));
+            }
+        }
+
+        public double Z
+        {
+            get => Zvalue;
+            set
+            {
+                Zvalue = value;
+                OnPropertyChanged(nameof(PrintValues));
             }
         }
 
