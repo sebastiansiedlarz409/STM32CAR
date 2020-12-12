73dd6403cf405a557e871203d94cd6eeff27af47
Sebastian Siedlarz <siedlarzsebastian409@gmail.com>
2020-12-12 10:25:23 +0100

    Images

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -597,7 +597,7 @@ static void MX_TIM4_Init(void)
 
   /* USER CODE END TIM4_Init 1 */
   htim4.Instance = TIM4;
-  htim4.Init.Prescaler = 800-1;
+  htim4.Init.Prescaler = 200-1;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim4.Init.Period = 1000-1;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
